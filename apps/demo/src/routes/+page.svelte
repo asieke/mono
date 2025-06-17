188490240ee527a6fa708a83eb9cac26ddf7541f
Alex Sieke <asieke@gmail.com>
2025-06-17 17:02:44 -0400

    scaffolding

---
+++ apps/demo/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import { Button } from '@repo/fidui';
+</script>
+
+<Button>Click me</Button>
---
+++ packages/fidui/src/components/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  /* PLACEHOLDER FOR BUTTON */
+  let { children } = $props();
+</script>
+
+<button>
+  {@render children()}
+</button>
+
+<style>
+  button {
+    background-color: #000;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+  }
+</style>
